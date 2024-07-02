84fbaf1d82000eda4523561f86f939d1622d32b6
Sokhna Sylla <syllasokhna2409@gmail.com>
2024-07-02 10:21:07 +0000

    result search

--- src/Components/Livres/ResultSearch.jsx
+++ src/Components/Livres/ResultSearch.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Box, Button, Card, CardContent, CardMedia, Grid, Typography,Alert } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Box, Button, Card, CardContent, CardMedia, Grid, Typography, Alert } from '@mui/material';
 
 const formatDate = (dateString) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -8,25 +7,34 @@ const formatDate = (dateString) => {
 };
 
 function ResultSearch({ livre }) {
+    const handleReload = () => {
+        window.location.reload();
+    };
+
     return (
         <Box sx={{ p: 2 }}>
             {livre.length === 0 ? (
-                <Alert variant="filled" severity="warning">
-                    Aucun livre trouvé , veuillez ressayer.
-                </Alert>
+                <>
+                    <Alert variant="filled" severity="warning">
+                        Aucun livre trouvé , veuillez ressayer.
+                    </Alert>
+                    <Button variant="contained" color="primary" onClick={handleReload} sx={{ mt: 2 }}>
+                        Accueil
+                    </Button>
+                </>
             ) : (
                 livre.map((result, index) => (
                     <Box key={index} sx={{ mb: 4 }}>
-                        <Card sx={{ display: 'flex', borderRadius: '15px', boxShadow: 3 }}>
+                        <Card sx={{ display: 'flex', flexDirection: 'column', borderRadius: '15px', boxShadow: 3 }}>
                             <CardMedia
                                 component="img"
-                                sx={{ width: 140, objectFit: 'contain' }}
+                                sx={{ width: '100%', height: 200, objectFit: 'contain' }}
                                 image={result.image && `data:image/png;base64,${result.image}`}
                                 alt={result.titre}
                             />
                             <CardContent sx={{ flex: 1 }}>
                                 <Grid container spacing={2}>
-                                    <Grid item xs={12} sm={6}>
+                                    <Grid item xs={12}>
                                         <Typography variant="h5" component="div" sx={{ mb: 2 }}>
                                             {result.titre}
                                         </Typography>
@@ -43,19 +51,18 @@ function ResultSearch({ livre }) {
                                             Date de Publication: {formatDate(result.datePublication)}
                                         </Typography>
                                     </Grid>
-                                    <Grid item xs={12} sm={6}>
-                                        {/* <Button
-                                            variant="contained"
-                                            color="primary"
-                                            component={Link}
-                                            to="/home"
-                                            sx={{ mt: 2 }}
-                                        >
-                                            Retour à l'accueil
-                                        </Button> */}
-                                    </Grid>
                                 </Grid>
                             </CardContent>
+                            <Box sx={{ textAlign: 'center', p: 2 }}>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={handleReload}
+                                    sx={{ mt: 2 }}
+                                >
+                                    Accueil
+                                </Button>
+                            </Box>
                         </Card>
                     </Box>
                 ))
